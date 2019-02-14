60f03601775d614448f88d8d4e4cfd825929425b
flywithbug <369495368@qq.com>
2019-02-15 04:04:37 +0800

    _

--- y_db_bash/db_data_migration_simple.sh
+++ y_db_bash/db_data_migration_simple.sh
@@ -7,5 +7,5 @@ mongodump -d  doc_manager -o ./db_backup/
 #拷贝数据库到远端文
 scp  -r ./db_backup/doc_manager name@host:/root/dump
 
-#远程恢复数据库内容   数据库地址和端口           本地数据库目录            数据库用户名 密码     数据库名    覆盖远端数据库
+#远程恢复数据库内容   数据库地址和端口  本地数据库目录      数据库用户名 密码     数据库名    覆盖远端数据库
 mongorestore -h host:port ./db_backup/doc_manager -u name -p pass -d docmanager  --drop
